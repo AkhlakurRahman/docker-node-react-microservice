56da20baa08ebecaea0625fb07403deba709dbde
AkhlakurRahman <akhlakur.rahmann@gmail.com>
2020-03-11 00:44:18 +0600

    configured api-gateway with graphql, node and express

---
+++ api-gateway/src/graphql/resolvers/Query/listings.js
@@ -0,0 +1,11 @@
+const listingsResolver = async () => {
+  return [
+    {
+      id: 1,
+      title: 'First title',
+      description: 'Hi description'
+    }
+  ];
+};
+
+export default listingsResolver;
